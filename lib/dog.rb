18a0ffb3b6f80d64da61b0d8bd58cbdf5ead36a9
CindyChenx <chen_cx0128@hotmail.com>
2019-06-05 17:29:04 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
 
     attri_reader :name
-    
+
     @@all = []
 
     def initialize(name)
@@ -10,12 +10,14 @@ class Dog
     end
 
     def self.all
-        Dog.all.each do |dog|
-            puts dog.name
+        # Dog.all.each do |dog|
+        #     puts dog.name
+        @@all.map |dog|
+            dog.name
     end
 
     def self.clear_all
-
+        @@all.clear
     end
 
 end # end Dog class
