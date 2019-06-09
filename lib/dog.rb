05bc5f9ed6fb7eca775fb36bdf04f1c10ae98610
JakubowskiA <arieljakubowski@gmail.com>
2019-06-09 17:34:55 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,28 +1,60 @@
 require "pry"
-
 class Dog
     @@all = []
-    @@names = []
-
     attr_reader :name
 
     def initialize(name)
-        @@all << self
         @name = name
-        @@names << name
+        @@all << self
     end
 
-    def name
-        @name
+    #binding.pry
+    def self.all
+        @@all.each do |dog|
+           puts dog.name
+        end
     end
 
     def self.clear_all
         @@all = []
     end
 
-    def self.all
-        @@names.uniq.each do |name|
-            puts name
-        end
-    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+# class Dog
+#     @@all = []
+#     @@names = []
+
+#     attr_reader :name
+
+#     def initialize(name)
+#         @@all << self
+#         @name = name
+#         @@names << name
+#     end
+
+#     def name
+#         @name
+#     end
+
+#     def self.clear_all
+#         @@all = []
+#     end
+
+#     def self.all
+#         @@names.uniq.each do |name|
+#             puts name
+#         end
+#     end
+# end
